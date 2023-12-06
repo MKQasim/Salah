0ccb8105776b0255382e60d6e1dd73b5aa68c709
Muhammad <kamqasim1@gmail.com>
2023-12-06 11:01:00 +0100

    Initial Commit

---
+++ Salah/SalahApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalahApp.swift
+//  Salah
+//
+//  Created by Qassim on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SalahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

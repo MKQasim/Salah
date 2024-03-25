534cbf87d64c2e63e6e50a34b1c1f56bc7729862
Muhammad Qasim Mubeen <kamqasim1@gmail.com>
2024-03-25 07:20:32 +0100

    [Muhammad] improved version tested.

--- Build/Intermediates.noindex/Salah.build/Debug/Salah.build/Objects-normal/arm64/PrayTime.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/test/iCloud\ Drive\ (Archive)/Documents/Documents\ -\ Test’s\ MacBook\ Pro/GitHub/Salah/Salah/Main/PrayTime.m \
-  /Users/test/iCloud\ Drive\ (Archive)/Documents/Documents\ -\ Test’s\ MacBook\ Pro/GitHub/Salah/Salah/Main/PrayTime.h
